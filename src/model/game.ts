5b444003877189df236833b5537392116e868d29
Iulia-Maria Ispas <119042517+IuliaIspas3@users.noreply.github.com>
2025-09-09 18:45:25 +0200

    changes on the game model logic

--- src/model/game.ts
+++ src/model/game.ts
@@ -1,5 +1,42 @@
-import { Round } from "./round";
+import { deck } from "./deck";
+import { PlayerHand } from "./playerHand";
+import { round, Round } from "./round";
 
-export type Game = {
-    rounds : Round[]
+export interface Game {
+    rounds : Round[],
+    currentRound: Round | undefined,
+    isGameFinished: boolean,
+
+    startGame(): void,
+    nextRound(): void,
+    finishGame(): void;
+}
+
+export function game(playerHands: PlayerHand[]): Game {
+    return {
+        rounds: [],
+        currentRound: undefined,
+        isGameFinished: false,
+
+        startGame() {
+            const roundDeck = deck();
+            const firstRound = round(playerHands, roundDeck)
+            this.rounds.push(firstRound);
+            this.currentRound = firstRound;
+        },
+
+        nextRound() {
+            //check if anyone has the score 500
+            if(this.currentRound?.isFinished) {
+                const roundDeck = deck();
+                //reset playerHands cards
+                const nextRound = round(playerHands, roundDeck);
+                this.rounds.push(nextRound);
+                this.currentRound = nextRound;
+            }
+        },
+        finishGame() {
+            
+        },
+    }
 }
